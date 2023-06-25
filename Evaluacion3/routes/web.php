989675563490fe4df181bac96a4497229a729fe8
benjamin-ortizc <benjamin.ortizc@icloud.com>
2023-06-25 16:51:42 -0400

    feat(routes): add home route

--- Evaluacion3/routes/web.php
+++ Evaluacion3/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ImagenController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+Route::get('/imagenes/upload', [ImagenController::class, 'upload'])->name('imagen.upload');
+Route::post('/imagenes/store', [ImagenController::class, 'store'])->name('imagen.store');
