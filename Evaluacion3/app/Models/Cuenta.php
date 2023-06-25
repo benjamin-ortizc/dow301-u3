49007a27bc81e098a0d915b1239d65873df9143f
benjamin-ortizc <benjamin.ortizc@icloud.com>
2023-06-25 16:41:13 -0400

    feat(models): add relations to models

--- Evaluacion3/app/Models/Cuenta.php
+++ Evaluacion3/app/Models/Cuenta.php
@@ -4,9 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Cuenta extends Model
 {
     use HasFactory;
     protected $table = 'cuentas';
+    protected $primaryKey = 'user';
+    protected $keyType = 'string';
+    public $incrementing = false;
+    public $timestamps = false;
+
+    public function perfil():BelongsTo
+    {
+        return $this->belongsTo(Perfil::class);
+    }
 }
--- Evaluacion3/app/Models/Perfil.php
+++ Evaluacion3/app/Models/Perfil.php
@@ -4,9 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Perfil extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $table = 'perfiles';
+    public $timestamps = false;
+
+    public function cuentas():HasMany
+    {
+        return $this->hasMany(Cuenta::class);
+    }
 }
